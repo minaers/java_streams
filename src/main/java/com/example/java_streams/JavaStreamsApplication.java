c70621843954ceca745f49c127609a17f8e6d97a
mwadea <minawadea731@gmail.com>
2022-02-09 13:12:29 +0200

    first

---
+++ src/main/java/com/example/java_streams/JavaStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStreamsApplication.class, args);
+    }
+
+}
